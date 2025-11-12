889f3a1f1625b6f9d76576a1247c2ecfcb4ee8ca
OleksandrDaniltsev <workchine2107@gmail.com>
2025-11-12 18:39:33 +0300

    Add missing User icon

--- src/components/mobile/lib/icons.jsx
+++ src/components/mobile/lib/icons.jsx
@@ -112,3 +112,9 @@ export const TrendingUp = ({ className }) => (
   </svg>
 );
 
+export const User = ({ className }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+  </svg>
+);
+
